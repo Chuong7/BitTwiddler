b9f3ff49ef84296adaa941b2297eec53f2c48ea0
ChuongNguyen7 <cmeister703@gmail.com>
2015-12-03 11:04:23 -0500

    Create Solutions_Chuong.c

---
+++ Solutions_Chuong.c
@@ -0,0 +1,248 @@
+/* 
+ * 
+ * solutions by: Chuong Nguyen 
+ * 
+ 
+ */
+
+#if 0
+
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+FLOATING POINT CODING RULES
+
+For the problems that require you to implent floating-point operations,
+the coding rules are less strict.  You are allowed to use looping and
+conditional control.  You are allowed to use both ints and unsigneds.
+You can use arbitrary integer and unsigned constants.
+
+You are expressly forbidden to:
+  1. Define or use any macros.
+  2. Define any additional functions in this file.
+  3. Call any functions.
+  4. Use any form of casting.
+  5. Use any data type other than int or unsigned.  This means that you
+     cannot use arrays, structs, or unions.
+  6. Use any floating point data types, operations, or constants.
+
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to 
+     check the legality of your solutions.
+  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+     that you are allowed to use for your implementation of the function. 
+     The max operator count is checked by dlc. Note that '=' is not 
+     counted; you may use as many of these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. Use the BDD checker to formally verify your functions
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies 
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
+
+/*
+ * STEP 2: Modify the following functions according the coding rules.
+ * 
+ */
+
+
+#endif
+/* 
+ * minusOne - return a value of -1 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 2
+ *   Rating: 1
+ */
+int minusOne(void) {
+  int a = ~0;
+  return a;
+}
+
+/* 
+ * fitsShort - return 1 if x can be represented as a 
+ *   16-bit, two's complement integer.
+ *   Examples: fitsShort(33000) = 0, fitsShort(-32768) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 8
+ *   Rating: 1
+ */
+ 
+/* hardest method in this lab. since corner cases were hard to 
+solve and mapping of outside parameters to binary representation 
+provided unexpected results. must be careful of 15 vs 16 >> shift. shift 
+single one out to create 1 and all 0s or invert it for opposite effect
+*/
+int fitsShort(int x) {
+
+
+	int a = ~0 << 15;
+	int c = a & x;
+	int d = c >> 15;
+	int e = d + 1;
+	int f = ~(a << 16);
+	int g = f << 1;
+	int h = g & e;
+	int i = !h;
+	return i;
+	
+}
+/* 
+ * upperBits - pads n upper bits with 1's
+ *  You may assume 0 <= n <= 32
+ *  Example: upperBits(4) = 0xF0000000
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 10
+ *  Rating: 1
+ */
+
+/* seems like ~0 a great place to start when specs call for filling in 1s. mistook
+subtraction sign as allowable instead of shifting
+*/
+int upperBits(int n) {
+  int a = ~0;
+  int b = !n + ~1 + 1;
+  int c = a & b;
+  int d = c << (32 + ~n + 1);
+  return d;
+}
+/* 
+ * anyOddBit - return 1 if any odd-numbered bit in word set to 1
+ *   Examples anyOddBit(0x5) = 0, anyOddBit(0x7) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 2
+ */
+ 
+// created alternating 1's and 0s and applied mask
+int anyOddBit(int x) {
+    
+    
+    int a = 170 << 24;
+    int b = 170 << 16;
+    int c = 170 << 8;
+    int d = 170;
+    int e = a + b + c + d;
+    int f = e & x;
+    return !!f;
+  
+}
+/* 
+ * implication - return x -> y in propositional logic - 0 for false, 1
+ * for true
+ *   Example: implication(1,1) = 1
+ *            implication(1,0) = 0
+ *   Legal ops: ! ~ ^ |
+ *   Max ops: 5
+ *   Rating: 2
+ */
+ 
+/* tried out different combinations to isolate the 1,0 combination with diff value
+than the rest 
+*/
+int implication(int x, int y) {
+
+    int a = (!x) | y;
+    return a;
+    
+       
+}
+/* 
+ * bitMask - Generate a mask consisting of all 1's 
+ *   lowbit and highbit
+ *   Examples: bitMask(5,3) = 0x38
+ *   Assume 0 <= lowbit <= 31, and 0 <= highbit <= 31
+ *   If lowbit > highbit, then mask should be all 0's
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+ 
+/* did the high < low scenario last. larger low value would result in 0 after double 
+shifts. mistakes resulted in trying to get highbit shift to position. forgot that 
+flipping the bits needed to be reflected in one place over when filling with 1s
+*/ 
+int bitMask(int highbit, int lowbit) {
+
+  int a = ~0;
+  int b = a << highbit;
+  int c = b << 1;
+  int d = ~c;
+  int e = a << lowbit;
+  int f = e & d;
+  int g = 1;
+  int h = g << highbit;
+  int i = h >> lowbit;
+  int j = !!i;
+  int k = ~j+1;
+  return f & k;
+  
+}
